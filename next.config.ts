8cd01ab48c4d78c831b4650d87c1c0a73c095716
Sadra Khosravi <sadrakh@outlook.com>
2024-12-08 22:29:50 -0800

    feat(nextjs-config): add image domains configuration to accept external sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coin-images.coingecko.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
